a2e3c131f2b1e4fe1762b0d8b2e8de1dd32ec0be
Lian930 <liulianken0930tw@gmail.com>
2025-06-28 16:29:24 +0800

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccount.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://highwaynotifier-default-rtdb.asia-southeast1.firebasedatabase.app" // ⬅️ 換成你的實際專案網址
-});
-
-module.exports = admin.database();

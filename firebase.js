9fdd847beb7b9efb53369325230bf468bc27b13f
Lian930 <liulianken0930tw@gmail.com>
2025-07-14 22:43:32 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,9 @@
 const admin = require("firebase-admin");
-const fs = require("fs");
-const raw = fs.readFileSync("serviceAccount.json", "utf-8");
 const json = require("./serviceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(json),
-  databaseURL: "https://highwaynotifier-default-rtdb.asia-southeast1.firebasedatabase.app" // 替換為你的
+  databaseURL: "https://highwaynotifier-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
 module.exports = admin.database();
-

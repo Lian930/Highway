cc8c093f156f13f77fc1d3799f8d7ad5a64f6aad
Lian930 <liulianken0930tw@gmail.com>
2025-07-14 22:41:54 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,5 @@
 const admin = require("firebase-admin");
 const fs = require("fs");
-console.log("所有 env 變數：", Object.keys(process.env));
-console.log("env內容開頭：", process.env.FIREBASE_SERVICE_ACCOUNT?.slice(0, 300));
-console.log("env內容長度：", process.env.FIREBASE_SERVICE_ACCOUNT?.length);
 const raw = fs.readFileSync("serviceAccount.json", "utf-8");
 const json = require("./serviceAccount.json");
 
